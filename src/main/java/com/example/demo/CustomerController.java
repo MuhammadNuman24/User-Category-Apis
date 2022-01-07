ed9fcf77638c73d811f2985052f1b1dbd316ff4d
MuhammadNuman24 <numanrazzaq24@gmail.com>
2022-01-07 10:18:12 +0500

    add controller

---
+++ src/main/java/com/example/demo/CustomerController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/customer")
+public class CustomerController {
+    @GetMapping("/list")
+    public List<CustomerModel>  getModel(){
+
+        return null;
+    }
+}
